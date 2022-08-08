aa8680893e09fb3537b13706f241c54fc8e593d7
jngriedel <jngriedel@gmail.com>
2022-08-08 15:55:47 -0700

    splash

--- react-app/src/components/Splash/Splash.js
+++ react-app/src/components/Splash/Splash.js
@@ -1,5 +1,7 @@
 import React, {  useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import logo from '../../images/logo.png'
 import './Splash.css'
 
 
@@ -13,10 +15,14 @@ function Splash(){
     return (
         <div className='splash-wrapper'>
             <div className='splash-header'>
-                Header
+                <img className='splash-logo' alt='logo' src={logo} />
+                <div className='splash-links'>
+                <NavLink className='splash-link' to='/sign-up'>Sign Up</NavLink>
+                <NavLink className='splash-link' to='/login'>Log In</NavLink>
+                </div>
             </div>
             <div className='splash-main'>
-                <h1>Music for you.</h1>
+                <h1 className='splash-message'>Music for you.</h1>
             </div>
         </div>
     )
