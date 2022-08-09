f8ad16563b4740b11582702c4507c4c6d53f93a8
jngriedel <jngriedel@gmail.com>
2022-08-09 10:37:12 -0700

    full home page

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -61,6 +61,62 @@ function Home() {
    ))}
     </div>
 
+    <h2>Rock</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Rock').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Electronic</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Electronic').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Rap</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Rap').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Jazz</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Jazz').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Metal</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Metal').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Country</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Country').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Classical</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Classical').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
+    <h2>Other</h2>
+    <div className='playlist-carousel scrollable'>
+    {allSongs && Object.values(allSongs).filter((song)=>  song.genre === 'Other').map((song, i)=>(
+    <SongPreview song={song} />
+   ))}
+    </div>
+
 
 
 
